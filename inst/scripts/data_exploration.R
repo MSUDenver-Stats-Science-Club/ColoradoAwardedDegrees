a552f18fb92d079181f1dab2095cbc7fb25a37d5
Jason Carey <jasoncareyco95@outlook.com>
2021-12-25 11:16:52 -0700

    Begin analyzing data

--- inst/scripts/data_exploration.R
+++ inst/scripts/data_exploration.R
@@ -20,3 +20,25 @@ ad_org <- app_data |>
     )
   )
 
+## I want to start looking at specific colleges to gather insights
+## Maybe we can look at time series of enrollment, total degrees awarded by university and university location, demographics, degree levels
+ad_count_by_uni <- ad_org |>
+  dplyr::group_by(
+    year,
+    institutionname
+  ) |>
+  dplyr::summarise(
+    total_awarded = sum(recordcount, na.rm = TRUE)
+  ) |>
+  dplyr::ungroup()
+
+ad_degree_levels <- ad_org |>
+  dplyr::group_by(
+    year,
+    # institutionname,
+    degreelevel
+  ) |>
+  dplyr::summarise(
+    total_awarded = sum(recordcount, na.rm = TRUE)
+  ) |>
+  dplyr::ungroup()
