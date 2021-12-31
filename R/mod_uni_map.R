6f164968c5fd9dc055e3317621a807d0db040ee3
Brady Lamson <blamson@gmail.com>
2021-12-31 15:34:16 -0700

    Basic shiny output implemented.

--- R/mod_uni_map.R
+++ R/mod_uni_map.R
@@ -7,22 +7,72 @@
 #' @noRd 
 #'
 #' @importFrom shiny NS tagList 
-# mod_uni_map_ui <- function(id){
-#   ns <- NS(id)
-#   tagList(
-#  
-#   )
-# }
+
+library(leaflet)
+mod_uni_map_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+    fluidPage(
+      uiOutput(ns("main"))
+    )
+  )
+}
     
 #' uni_map Server Functions
 #'
 #' @noRd 
-# mod_uni_map_server <- function(id){
-#   moduleServer( id, function(input, output, session){
-#     ns <- session$ns
-#  
-#   })
-# }
+mod_uni_map_server <- function(id, rv){
+  moduleServer( id, function(input, output, session){
+    ns <- session$ns
+    
+    ## Reactives ---
+    
+    ## Experimental Code ---
+    
+    ## Read in location dataset
+    location_df <- readRDS("data/institution_locations.rds")
+    
+    ## Create map object
+    colorado_counties <- maps::map("county", 'colorado', fill = TRUE, plot = FALSE)
+    
+    ## Render UI ---
+    output$main <- renderUI({
+      tagList(
+        leafletOutput(ns("coloradoMap"))
+      )
+    })
+    
+    ## generate map output
+    output$coloradoMap <- renderLeaflet({
+      
+      leaflet(data = colorado_counties, 
+              options = leafletOptions(
+                minZoom = 6.4,
+                maxZoom = 12.5)
+      ) |>
+        addProviderTiles(providers$Thunderforest.MobileAtlas) |>
+        addProviderTiles(providers$Stamen.TonerLines,
+                         options = providerTileOptions(opacity = 0.35)) |>
+        addProviderTiles(providers$Stamen.TonerLabels) |>
+        addMarkers(~location_df$long, 
+                   ~location_df$lat, 
+                   popup = location_df$institutionname) |>
+        addPolygons(
+          color = "#444444",
+          weight = 1.5,
+          smoothFactor = 1,
+          fillColor = RColorBrewer::brewer.pal(12, "Paired"),
+          stroke = TRUE,
+          highlightOptions = highlightOptions(
+            stroke = TRUE,
+            color = "white",
+            weight = 5,
+            bringToFront = TRUE)
+        )
+    })
+
+  })
+}
     
 ## To be copied in the UI
 # mod_uni_map_ui("uni_map_ui_1")
@@ -31,35 +81,3 @@
 # mod_uni_map_server("uni_map_ui_1")
 
 ### Working on bringing everything over. 
-
-#library(leaflet)
-location_df <- readRDS("data/institution_locations.rds")
-
-colorado_counties <- maps::map("county", 'colorado', fill = TRUE, plot = FALSE)
-
-
-leaflet(data = colorado_counties, 
-        options = leafletOptions(
-          minZoom = 7,
-          maxZoom = 12.5)
-) |>
-  addProviderTiles(providers$Thunderforest.MobileAtlas) |>
-  addProviderTiles(providers$Stamen.TonerLines,
-                   options = providerTileOptions(opacity = 0.35)) |>
-  addProviderTiles(providers$Stamen.TonerLabels) |>
-  addMarkers(~location_df$long, 
-             ~location_df$lat, 
-             popup = location_df$institutionname) |>
-  addPolygons(
-    color = "#444444",
-    weight = 1.5,
-    smoothFactor = 1,
-    fillColor = RColorBrewer::brewer.pal(12, "Paired"),
-    stroke = TRUE,
-    highlightOptions = highlightOptions(
-      stroke = TRUE,
-      color = "white",
-      weight = 5,
-      bringToFront = TRUE)
-  )
-
