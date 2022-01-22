5c61ea13c2dd2a93154fa969436759f6027e9305
Jason Carey <jasoncareyco95@outlook.com>
2022-01-22 12:49:21 -0700

    Package check updates

---
+++ R/ColoradoAwardedDegrees-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+utils::globalVariables(
+  c(
+    "agemax", "agemin", "app_data", "cip", "cip2", "cobased", "division", "institutionlevelid", "institutionname", "institutiontype", "programname", "recordcount", "residencyid", "taxtype", "total_awarded", "year"
+  )
+)
